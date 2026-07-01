5cb59645f74fa8cc4459aba5d22bbecb8239d681
Nishaa2729 <nnishakumari81@gmail.com>
2026-07-01 18:22:50 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+ "use strict"; 
+
+
+
+ let name = "Nishaa"
+ let agr = 18
+ let isLoggedIn = false
+
+//  number => to power 53
+//  bigint 
+//  string => "
+// boolean => true/false
+// null => standalone value
+// undefined => meaing value is not assigned
+// symbol => unique
+
+// obect
+
+console.log(typeof null);        // undefined
+console.log(typeof undefined);   // object
