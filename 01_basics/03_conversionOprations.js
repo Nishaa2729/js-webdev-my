ce539535334bad157c4d74c7ee7dc2c097352f2b
Nishaa2729 <nnishakumari81@gmail.com>
2026-07-03 08:28:42 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOprations.js
@@ -0,0 +1,26 @@
+let score = true
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abch" => Nan
+//  true => 1; false => 0
+
+let isLoggedIn = true
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+//  1 => true; 0 => false
+// "" => false
+// "nishaa" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
