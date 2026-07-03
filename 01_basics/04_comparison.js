9a9ab3a341de752db185c842e2ad8420558c5a6b
Nishaa2729 <nnishakumari81@gmail.com>
2026-07-03 09:10:59 +0000

    comparsions

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);  
+// one is string and the other one number 
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);    
+// AVOID THESE KIND COMPARISONS OF NULL AND UNDEFINED ONE**********
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+//  ===
+console.log("2" === 2);
+// TRIPLE === MEANS STRICT CHECKING
