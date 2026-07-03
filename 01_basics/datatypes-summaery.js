e39bb5471261ed5fbb9f7fb296ef255b662e09c9
Nishaa2729 <nnishakumari81@gmail.com>
2026-07-03 09:27:20 +0000

    datatypesummary

---
+++ 01_basics/datatypes-summaery.js
@@ -0,0 +1,21 @@
+// primitive
+// 7 types : String, Number, Boolean, Null, undefined, Symbol, BigInt
+
+
+
+// reference types is also called non primituve
+//  Array, Objects, Functions
+
+// array
+const heros = ["shaktiman", "nagraj", "crimemastergogo"];
+// curly bracket ke andar everthing of object and we write it as
+let myObj = {
+    name: "nisha",
+    age: 22,
+}
+
+// FUNCTIONS
+const myFunction = function(){
+    console.log("Hey buddy how are you");
+    
+}
